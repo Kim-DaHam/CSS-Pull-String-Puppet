a984f7e8b0bdf21652f1e07d32456f97ce5ac104
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-05-21 00:32:14 +0900

    feature/브라우저 크기에 따라 캔버스 크기 바꾸기 완료

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,44 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useMemo, useCallback } from 'react';
 import styled from "styled-components"
 import './App.css';
 
-const Canvas = styled.canvas`
-  width: 500px;
-  height: 500px;
-  border: solid red;
-`;
-
 function App() {
   const canvas = useRef(null);
-  const [ctx, setCtx] = useState();
+  const [ctx, setCtx] = useState(null);
+  const [pixelRatio, setPixelRatio] = useState(0);
+
+  const delay = 300;
+  let timer = null;
+
+  const resize = ()=>{
+    clearTimeout(timer);
+    timer = setTimeout(()=>{
+      const stageWidth = document.body.clientWidth;
+      const stageHeight = document.body.clientHeight;
+
+      canvas.width = stageWidth * pixelRatio;
+      canvas.height = stageHeight * pixelRatio;
+      ctx.scale(pixelRatio, pixelRatio);
+    }, delay)
+  }
 
   useEffect(() => {
     const context = canvas.current.getContext('2d');
     setCtx(context);
-  });
+    setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1)
+  },[]);
 
+  useEffect(()=>{
+    if(ctx !== null && pixelRatio !== 0) {
+      window.addEventListener('resize', resize, false);
+      resize();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ctx, pixelRatio])
 
   return (
     <div className="App">
-      <Canvas ref={canvas}></Canvas>
+      <canvas ref={canvas} style={{border:'red solid'}}></canvas>
     </div>
   );
 }
