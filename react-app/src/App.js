fabdd4113feec5797dbeab60fcdee7322396d0e2
Kim-DaHam <81691456+Kim-DaHam@users.noreply.github.com>
2023-05-20 18:55:26 +0900

    feat/styled-component 설치 및 Canvas 컴포넌트 생성

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,26 @@
+import { useRef, useState, useEffect } from 'react';
+import styled from "styled-components"
 import './App.css';
 
+const Canvas = styled.canvas`
+  width: 500px;
+  height: 500px;
+  border: solid red;
+`;
+
 function App() {
+  const canvas = useRef(null);
+  const [ctx, setCtx] = useState();
+
+  useEffect(() => {
+    const context = canvas.current.getContext('2d');
+    setCtx(context);
+  });
+
+
   return (
     <div className="App">
-
+      <Canvas ref={canvas}></Canvas>
     </div>
   );
 }
